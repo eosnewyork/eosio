7083bfe30f5193805e7515dd260dee549134f9ab
Buddy Deck <deck@eosnewyork.io>
2018-04-28 16:45:59 +0000

    add first pass at using eoscanada's bios process

---
+++ tests/roles/eosio/bios/files/contracts/eosio.system/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/deck/git/eos/contracts/eosio.system
+# Build directory: /home/deck/git/eos/build/contracts/eosio.system
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(validate_eosio.system_abi "/home/deck/git/eos/build/scripts/abi_is_json.py" "/home/deck/git/eos/contracts/eosio.system/eosio.system.abi")
