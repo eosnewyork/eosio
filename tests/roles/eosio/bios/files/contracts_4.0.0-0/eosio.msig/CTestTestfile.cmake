de599d66f0898f9bfe7d873f6909e98372578df6
Buddy Deck <deck@localhost.localdomain>
2018-05-25 07:02:15 -0400

    updating quite a bit and I don't feel like documenting it all

---
+++ tests/roles/eosio/bios/files/contracts_4.0.0-0/eosio.msig/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/deck/git/eos/contracts/eosio.msig
+# Build directory: /home/deck/git/eos/build/contracts/eosio.msig
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(validate_eosio.msig_abi "/home/deck/git/eos/build/scripts/abi_is_json.py" "/home/deck/git/eos/contracts/eosio.msig/eosio.msig.abi")
